65748125a1ff23fec44d8dfc9963197bb109f245
Alvaro Lopez <alvlopzam@icloud.com>
2025-05-22 15:55:44 -0600

    Rd v0

---
+++ Codigo/rd_trial.R
@@ -0,0 +1,10 @@
+############################## RDs ############################## 
+
+# Ajustar el path como corresponda
+setwd("/Users/Alvlopzam/Desktop/ITAM/Semestre 10/PolPubII/HoyNoCircula/Codigo")
+
+# Lectura de datos
+data_ma <- read.csv("../Datos/procesados/promedios_horas_moving_avg_24h.csv")
+
+
+
